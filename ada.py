4bd54abd3b1659a36abe8d11e93fff611660621b
tomasmartinsaldias <tomasmartin.saldias@gmail.com>
2025-08-25 22:53:02 -0300

    Add files via upload

--- ada.py
+++ ada.py
@@ -1,3 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(first_name.lower() + " " + last_name.lower())
+    print(first_name.lower().title() + " " + last_name.lower().title())
+    print(first_name.upper() + " " + last_name.upper())
+    print("\t" + first_name.lower() + " " + last_name.lower())
--- change.py
+++ change.py
@@ -1,3 +1,10 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto_pesos = int(money-expense)
+    vuelto_centavos = int((money-expense-vuelto_pesos)*100)
+    print(f"Ingresar gasto:\n{expense}")
+    print(f"Dinero recibido\n{money}")
+    print("\nVuelto\n")
+    print(f"Pesos:\n{vuelto_pesos}")
+    print(f"Centavos:\n{vuelto_centavos}")
